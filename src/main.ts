96e0e253bb902171bd10508f28e9c2b6251d3a74
rochaeduardo997 <rochaeduardo997@protonmail.com>
2022-01-24 15:06:24 -0300

    Removing useless import from main file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TabuadeMareService } from './tabuaDeMare/tabuaDeMare.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
